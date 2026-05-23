e5922b2feaa01f956b9acbbfcd76ad9e625bcd9b
Malek <malek.ma.itani@gmail.com.com>
2026-05-23 21:32:42 +0300

    Configure Next app for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
-const repo = "PRINCIPIA-STATISTICA-WEB";
-
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"]
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 const withMDX = createMDX({
-  extension: /\.mdx?$/
+  extension: /\.mdx?$/,
 });
 
 export default withMDX(nextConfig);
